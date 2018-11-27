549e93fb3e2246f2113e773e59a45a527ed6c9ae
dynamikapps <projects@dynamikapps.com>
2018-11-27 17:18:31 -0500

    commit all

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class App extends Component {
-  render() {
-    return <div className="App">Thanks We Are Ready!</div>;
-  }
-}
-
-export default App;
---
+++ src/components/ExpensifyApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ExpensifyApp extends Component {
+  render() {
+    return <div className="App">Expensify App Ready!</div>;
+  }
+}
+
+export default ExpensifyApp;
